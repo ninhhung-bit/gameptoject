8b0379107585e60d129c8f88b7b2b2981b70973a
ninhhung-bit <24021504@vnu.edu.vn>
2025-04-10 22:32:49 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,17 +9,67 @@
 
 
 #include <iostream>
+#include<cstdlib>
+#include<algorithm>
+#include<ctime>
 
 using namespace std;
 
 const int SIZE = 4;
-int board[SIZE][SIZE] = {
-    {1, 0, 0, 4},
-    {0, 4, 0, 0},
-    {0, 0, 4, 3},
-    {4, 0, 0, 1}
+int baseBoard[SIZE][SIZE] = {
+    {1, 2, 3, 4},
+    {3, 4, 1, 2},
+    {2, 1, 4, 3},
+    {4, 3, 2, 1}
 };
+int fullBoard[4][4];
+int board[4][4];
+
+void ngaunhienBoard( int board[4][4]){
+
+    for(int i=0;i<4;i+=2){
+        if(rand()%2) swap(board[i],board[i+1]);
+    }
+
+    for (int j = 0; j < 4; j += 2) {
+        if (rand() % 2) {
+            for (int i = 0; i < 4; i++) {
+                swap(board[i][j], board[i][j+1]);
+            }
+        }
+    }
+    int map[5]={0,1,2,3,4};
+    random_shuffle(map+1,map+5);
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++)
+        {
+            board[i][j]= map[board[i][j]];
+        }
+    }
+}
+
+void anchuso( int src[4][4], int dst[4][4], int blanks=6){
+
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<4;j++)
+        {
+            dst[i][j]=src[i][j];
+        }
+    }
+
+    while (blanks>0){
+        int r=rand()% 4;
+        int c=rand()% 4;
+        if(dst[r][c] !=0 ){
+            dst[r][c]=0;
+            blanks--;
+        }
+    }
+}
+
 void printBoard() {
+    system("cls");
 
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -53,21 +103,21 @@ bool isValid(int row, int col, int num) {
 void playerMove()
  {
     int row, col, num;
-    cout << "nhap hang (0-3), cot (0-3), so (1-4): ";
+    cout << "nhap hang (1-4), cot (1-4), so (1-4): ";
     cin >> row >> col >> num;
 
-    if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == 0)
+    if (row >= 1 && row < SIZE+1 && col >= 1 && col < SIZE+1 && board[row-1][col-1] == 0)
     {
-        if (isValid(row, col, num))
+        if (isValid(row-1, col-1, num))
           {
-            board[row][col] = num;
-          } else
-          {
+            board[row-1][col-1] = num;
+          } else{
             cout << "so khong hop le"<<endl;
+
           }
-    } else
-      {
+    } else{
         cout << "o nay khong the thay doi!"<< endl;
+
       }
 }
 
@@ -81,12 +131,19 @@ bool isGameWon()
 
 int main()
 {
+    srand(time (0));
+
+    memcpy(fullBoard, baseBoard, sizeof(baseBoard));
+    ngaunhienBoard(fullBoard);
+    anchuso(fullBoard, board,6);
+
     while(!isGameWon())
     {
         printBoard();
         playerMove();
+
     }
-    cout<< "Chúc mừng!!"<< endl;
+    cout<< "Chuc mungg bann!!"<<endl;
     return 0;
 }
 
